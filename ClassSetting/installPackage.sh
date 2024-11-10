d2777dbfd3ebd9d51b81371e9dbe528bb7c85ab1
R SSAEM <96120477+jetsonai@users.noreply.github.com>
2024-11-10 16:01:14 +0900

    Add files via upload

---
+++ ClassSetting/installPackage.sh
@@ -0,0 +1,19 @@
+sudo apt update -y 
+
+sudo apt-get -y install python3-pip curl net-tools samba git  
+sudo apt-get -y install g++ cmake cmake-gui doxygen mpi-default-dev openmpi-bin openmpi-common
+sudo apt-get -y install libeigen3-dev
+sudo apt-get -y install libusb-1.0-0-dev libqhull* libusb-dev libgtest-dev
+sudo apt-get -y install git-core freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev
+sudo apt-get -y install libphonon-dev libphonon-dev phonon-backend-gstreamer
+sudo apt-get -y install qt5-default
+sudo apt-get -y install flex bison
+sudo apt-get -y install libflann-dev libboost-all-dev libusb-1.0-0-dev libqhull-dev libpcap0.8-dev
+sudo apt-get -y install libvtk7-dev -y
+
+python3 -m pip install gdown 
+python3 -m pip install ultralytics 
+python3 -m pip install bertopic hdbscan 
+python3 -m pip install torchmetrics 
+python3 -m pip install bertopic
+python3 -m pip install hdbscan
